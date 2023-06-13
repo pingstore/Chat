05c76ac40716f54dfb317efb3e7ed900b017cf19
pingstore <136500985+pingstore@users.noreply.github.com>
2023-06-14 01:18:25 +0530

    Update ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div className="background">
       <PrettyChatWindow
-        projectId={"5e9d6cbc-cc6d-4924-a971-7dd8ed0cbe74"}
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
         username={props.user.username}
         secret={props.user.secret}
       />
