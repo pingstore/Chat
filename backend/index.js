7b4aefa83f07f1d1e47f488a8742d40c2d78d7d2
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2023-06-13 23:55:35 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const axios = require("axios");
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from Backend!'
+  })
+})
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key":"4388096e-ccad-4f75-9cf7-7a46db8a46b7" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001, () => console.log('AI server started on http://localhost:3001'));
